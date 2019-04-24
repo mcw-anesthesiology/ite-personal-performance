11198d73d2a967e5328beb51ffec9cbf65e932ff
Jacob Mischka <jacob@mischka.me>
2019-04-24 18:27:28 -0500

    Dump missed topics to csv

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use regex::Regex;
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::env;
+use std::error::Error;
 use std::fs::read_to_string;
+use std::io;
 
 mod constants;
 
@@ -21,12 +23,13 @@ fn main() {
 
     let mut trainee: Option<&mut Trainee> = None;
 
-    let all_items: Vec<&&str> = BASIC_SCIENCES_ITEMS
+    let all_items: Vec<&str> = BASIC_SCIENCES_ITEMS
         .iter()
         .chain(CLINICAL_SCIENCES_ITEMS.iter())
         .chain(ORGAN_BASED_SCIENCES_ITEMS.iter())
         .chain(CLINICAL_SUBSPECIALTIES_ITEMS.iter())
         .chain(SPECIAL_PROBLEMS_ITEMS.iter())
+        .map(|s| *s)
         .collect();
 
     for line in contents.lines() {
@@ -41,14 +44,14 @@ fn main() {
                 trainee = Some(trainees.entry(id).or_insert(Trainee {
                     name,
                     id,
-                    missed_topics: Vec::new(),
+                    missed_topics: HashSet::new(),
                 }));
             }
         } else {
             if let Some(trainee) = trainee.as_mut() {
                 for item in all_items.iter() {
-                    if line == **item {
-                        trainee.missed_topics.push(line.to_string());
+                    if line == *item {
+                        trainee.missed_topics.insert(line.to_string());
                     }
                 }
             }
@@ -56,11 +59,38 @@ fn main() {
     }
 
     dbg!(&trainees);
+
+    let mut trainees: Vec<Trainee> = trainees.drain().map(|(_, v)| v).collect();
+
+    trainees.sort_unstable_by(|a, b| a.name.cmp(&b.name));
+
+    dump_missed_topics(&trainees, &all_items).unwrap();
+}
+
+fn dump_missed_topics(trainees: &Vec<Trainee>, items: &Vec<&str>) -> Result<(), Box<Error>> {
+    let mut writer = csv::Writer::from_writer(io::stdout());
+    writer.write_field("Name")?;
+    writer.write_record(items.iter())?;
+
+    for trainee in trainees.iter() {
+        writer.write_field(&trainee.name)?;
+        writer.write_record(items.iter().map(|item| {
+            if trainee.missed_topics.contains(*item) {
+                "x"
+            } else {
+                ""
+            }
+        }))?;
+    }
+
+    writer.flush()?;
+
+    Ok(())
 }
 
 #[derive(Debug)]
 struct Trainee {
     name: String,
     id: u32,
-    missed_topics: Vec<String>,
+    missed_topics: HashSet<String>,
 }
